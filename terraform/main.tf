0fe02c3f183a8b8035561a448fa9eaa54c614a2f
donovanjd <donovandurand@gmail.com>
2022-06-15 00:15:54 +1000

    import existing control tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,31 +1,3 @@
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "donovandurand+aftdev3@gmail.com"
-    AccountName               = "aft-dev-3"
-    ManagedOrganizationalUnit = "AVX AFT"
-    SSOUserEmail              = "donovandurand@gmail.com"
-    SSOUserFirstName          = "Donovan"
-    SSOUserLastName           = "Durand"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
 module "import-aft-account" {
   source = "./modules/aft-account-request"
 
